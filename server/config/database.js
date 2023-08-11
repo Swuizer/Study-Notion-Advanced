bbc9011be2a8149e387ac52bf9f6abc928ffc2bf
Swuizer <arghya143ghosh@gmail.com>
2023-08-11 22:11:53 +0530

    First Commit StudyNotion

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { MONGODB_URL } = process.env;
-
 exports.connect = () => {
-    mongoose.connect(MONGODB_URL, {
+    mongoose.connect(process?.env?.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology:true,
     })
