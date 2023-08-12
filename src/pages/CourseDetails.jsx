421c5aedc0b99e0a3789ded12439d564ec728d95
Swuizer <arghya143ghosh@gmail.com>
2023-08-12 14:26:39 +0530

    First Commit StudyNotion

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -15,6 +15,7 @@ import RatingStars from '../components/common/RatingStars'
 import CourseDetailsCard from '../components/core/Course/CourseDetailsCard'
 import CourseAccordionBar from '../components/core/Course/CourseAccordionBar'
 import ConfirmationModal from "../components/common/ConfirmationModal"
+// import map from './map'; 
 
 const CourseDetails = () => {
 
@@ -122,80 +123,8 @@ const CourseDetails = () => {
         )
     }
 
-    {console.log("Course Data Time Duration", courseData?.data[0]?.courseContent?.[0]?.subSection?.[0]?.timeDuration)}
-    {console.log("Course Data Time Duration", courseData)}
-
-    // <div className='flex items-center ml-10'>
-    //     <button className='bg-yellow-50 px-6 py-3 mt-10'
-    //     onClick={() => handleBuyCourse()}
-    //     >
-    //         Buy Now
-    //     </button>
-    // </div>
-
-
-    // <div className={`relative w-full bg-richblack-800`}>
-    //         {/* Hero Section */}
-    //         <div className='mx-auto box-content px-4 lg:w-[1260px] 2xl:relative'>
-    //             {/* First box */}
-    //             <div className='mx-auto grid min-h-[450px] max-w-maxContentTab justify-items-center py-8 lg:mx-0 lg:justify-items-start lg:py-0 xl:max-w-[810px]'>
-    //                 {/* One */}
-    //                 <div className='relative block max-h-[30rem] lg:hidden'>
-    //                     <div className='absolute bottom-0 left-0 h-full w-full shadow-[#161D29_0px_-64px_36px_-28px_inset]'></div>
-    //                     <img
-    //                         src={thumbnail}
-    //                         alt='course thumbnail'
-    //                         className='aspect-auto w-full'
-    //                     />
-    //                 </div>
-    //                 {/* Two */}
-    //                 <div className={`z-30 my-5 flex flex-col justify-center gap-4 py-5 text-lg text-richblack-5`}>
-    //                     <div>
-    //                         <p className="text-4xl font-bold text-richblack-5 sm:text-[42px]">{courseName}</p>
-    //                     </div>
-    //                     <p className={`text-richblack-200`}>{courseDescription}</p>
-    //                     <div className='text-md flex flex-wrap items-center gap-2'>
-    //                         <span className='text-yellow-25'>{avgReviewCount}</span>
-    //                         <RatingStars Review_Count={avgReviewCount} Star_Size={24} />
-    //                         <span>{`(${ratingAndReviews.length} reviews)`}</span>
-    //                         <span>{`${studentsEnroled.length} students enrolled`}</span>
-    //                     </div>
-    //                     <div>
-    //                         <p>
-    //                             Created By {`${instructor.firstName} ${instructor.lastName}`}
-    //                         </p>
-    //                     </div>
-    //                     <div className='flex flex-wrap gap-5 text-lg'>
-    //                         <p className='flex items-center gap-2'>
-    //                             {" "}
-    //                             <BiInfoCircle/> Created at {formatDate(createdAt)}
-    //                         </p>
-    //                         <p className='flex items-center gap-2'>
-    //                             {" "}
-    //                             <HiOutlineGlobeAlt /> English
-    //                         </p>
-    //                     </div>
-    //                 </div>
-    //                 {/* Three */}
-    //                 <div className='flex w-full flex-col gap-4 border-y border-y-richblack-500 py-4 lg:hidden'>
-    //                     <p className='space-x-3 pb-4 text-3xl font-semibold text-richblack-5'>
-    //                         Rs. {price}
-    //                     </p>
-    //                     <button className='yellowButton' onClick={handleBuyCourse}>
-    //                         Buy Now
-    //                     </button>
-    //                     <button className='blackButton'>Add to Cart</button>
-    //                 </div>
-    //             </div>
-    //             {/* Course Card */}
-    //             <div className='right-[1rem] top-[60px] mx-auto hidden min-h-[600px] w-1/3 max-w-[410px] translate-y-24 md:translate-y-0 lg:absolute lg:block'>
-    //                 {/* <CourseDetailsCard/> */}
-    //             </div>
-    //         </div>
-    //     </div>
-    //     <div></div>
-    //     <Footer/>
-
+    // {console.log("Course Data Time Duration", courseData?.data[0]?.courseContent?.[0]?.subSection?.[0]?.timeDuration)}
+    // {console.log("Course Data Time Duration", courseData)}
 
 
   return (
@@ -343,71 +272,3 @@ const CourseDetails = () => {
 }
 
 export default CourseDetails
-
-{/* <>
-<div className='relative flex flex-col justify-start'>
-    <p>{courseName}</p>
-    <p>{courseDescription}</p>
-    <div>
-        <span>{avgReviewCount}</span>
-        <RatingStars Review_Count={avgReviewCount} Star_Size={24} />
-        <span>{`(${ratingAndReviews.length} reviews)`}</span>
-        <span>{`(${studentsEnrolled.length} Students Enrolled)`}</span>
-    </div>
-    <div>
-        <p>Created By {`${instructor.firstName}`}</p>
-    </div>
-    <div>
-        <p>
-            Created At {formatDate(createdAt)}
-        </p>
-        <p>
-            {" "}
-            English
-        </p>
-    </div>
-
-    <div>
-        <CourseDetailsCard 
-            course={courseData?.data[0]}
-            setConfirmationModal={setConfirmationModal}
-            handleBuyCourse={handleBuyCourse}
-        />
-    </div>
-
-</div>
-
-<div>
-    <p> What you'll learn </p>
-    <div>
-        {whatYouWillLearn}
-    </div>
-</div>
-
-<div>
-    <div>
-        <p>Course Content:</p>
-    </div>
-
-    <div className='flex gap-x-3 justify-between'>
-        <div>
-            <span>{courseContent.length} section(s)</span>
-            
-            <span>
-                {totalNoOfLectures} lectures
-            </span>
-            <span>
-                {courseData.data[0]?.totalDuration} total duration
-            </span>
-        </div>
-        <div>
-            <button
-                onClick={() => setIsActive([])}
-            >
-                Collapse all Section
-            </button>
-        </div>
-    </div>
-</div>
-
-</> */}
