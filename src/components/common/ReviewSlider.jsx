508ec108487a107c7e36ca22ee994f4ff10ea4a1
Swuizer <arghya143ghosh@gmail.com>
2023-08-14 17:41:30 +0530

    First Commit StudyNotion

--- src/components/common/ReviewSlider.jsx
+++ src/components/common/ReviewSlider.jsx
@@ -34,7 +34,7 @@ const ReviewSlider = () => {
                 loop={true}
                 freeMode={true}
                 autoplay={{
-                    delay: 2500,
+                    delay: 1500,
                     disableOnInteraction: false,
                 }}
                 modules={[FreeMode, Pagination, Autoplay]}
