1e1b3f40ee88b9a08dcb2dbf5561b70a53c2c945
Swuizer <arghya143ghosh@gmail.com>
2023-08-14 17:21:32 +0530

    First Commit StudyNotion

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -46,7 +46,7 @@ const LearningGridArray = [
 
 const LearningGrid = () => {
   return (
-    <div className='grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12'>
+    <div className='grid mx-auto w-[90%] xl:w-fit grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-12'>
         {
             LearningGridArray.map( (card, index) => {
                 return (
