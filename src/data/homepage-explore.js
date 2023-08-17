25de1792275eb467c62f878cb3887a7f0e634061
Swuizer <arghya143ghosh@gmail.com>
2023-08-17 23:37:02 +0530

    First Commit StudyNotion

--- src/data/homepage-explore.js
+++ src/data/homepage-explore.js
@@ -50,19 +50,19 @@ export const HomePageExplore = [
         courses : [
             {
                 heading : "Java",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                description : "This course introduces fundamental Java concepts, covering syntax, data types, control structures, object-oriented principles, and application development",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "Python",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                description : "This program teaches fundamental Python concepts, enabling coding proficiency in data manipulation, automation, algorithms, and software development",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "SCSS",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                description : "This course introduces SCSS, teaching nested styles, variables, mixins, and efficient CSS compilation for enhanced web design",
                 level : 'Beginner',
                 lessionNumber : 6
             },
@@ -73,19 +73,19 @@ export const HomePageExplore = [
         courses : [
             {
                 heading : "Flask",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                description : "Flask teaches Python web app development, offering simplicity to create dynamic sites with routing, templates, forms, and extensions.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "Django",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                description : "Django course teaches creating web apps with Python, covering models, views, templates, authentication, and database integration for modern development",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "Fast API",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                description : "FastAPI course covers creating APIs in Python with high performance, automatic validation, and interactive documentation for modern web applications",
                 level : 'Beginner',
                 lessionNumber : 6
             },
@@ -96,19 +96,19 @@ export const HomePageExplore = [
         courses : [
             {
                 heading : "Next.js",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                description : "Next.js teaches web development with React, providing server-rendered, SEO-friendly, and efficient client-side routing for modern web applications",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "Nuxt.js",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                description : "Nuxt.js course: Learn to build modern web applications with Vue.js, server-side rendering, routing, and state management for enhanced user experiences",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
                 heading : "Sanity",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                description : "Sanity course covers essential HTML concepts: page structure, text, links, images, providing foundational knowledge for web development",
                 level : 'Beginner',
                 lessionNumber : 6
             },
